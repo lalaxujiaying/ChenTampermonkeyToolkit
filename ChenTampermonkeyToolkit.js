1b0fecccf1487c4e71aaa7fa92193b19dbfc2180
lalaxujiaying <648432889@qq.com>
2026-01-26 18:29:26 +0800

    测试油猴自动更新功能

--- ChenTampermonkeyToolkit.js
+++ ChenTampermonkeyToolkit.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChenTampermonkeyToolkit
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  自用chrome网页脚本工具
 // @author       Chen
 // @match		https://www.bilibili.com/video/*
